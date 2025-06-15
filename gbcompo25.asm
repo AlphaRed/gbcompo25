196c0b859ba24467fe84ed7582e97ba149c78b69
Isaac Volk <isaacvolk3@gmail.com>
2025-06-14 23:11:03 -0400

    It works now

--- gbcompo25.asm
+++ gbcompo25.asm
@@ -1,45 +1,60 @@
+.GBHEADER
+	NINTENDOLOGO
+	NAME "TEST"
+	ROMDMG
+	LICENSEECODENEW "XX"
+	CARTRIDGETYPE $00
+	RAMSIZE $00
+	COUNTRYCODE $01
+.ENDGB
+
 .MEMORYMAP
-DEFAULTSLOT 0
-SLOTSIZE $4000
-SLOT 0 $0000
-SLOT 1 $4000
+	DEFAULTSLOT 0
+	SLOTSIZE $4000
+	SLOT 0 $0000
+	SLOT 1 $4000
 .ENDME
 
 .ROMBANKMAP
-BANKSTOTAL 2
-BANKSIZE $4000
-BANKS 2
+	BANKSTOTAL 2
+	BANKSIZE $4000
+	BANKS 2
 .ENDRO
 
-.BANK 0 SLOT 0
-
-.ORG $0000
-; rst instructions here
-; interrupts...to figure out later
-.ORG $0040
-	reti
-.ORG $0048
-	reti
-.ORG $0050
-	reti
-.ORG $0058
-	reti
-.ORG $0060
-	reti
-
-
-
+.BANK 0
 .ORG $0100
-nop
-jp start
+	nop
+	jp start
 
-.NINTENDOLOGO
-.NAME "WALLYS WORLD"
-.LICENSEECODENEW "00"
-.CARTRIDGETYPE 1
-.RAMSIZE 0
-.COMPUTEGBCHECKSUM
-.COMPUTEGBCOMPLEMENTCHECK
-
-.ORG$0150
+.ORG $0150
 start:
+	di
+	ld sp, $FFFE
+	
+	ld a, %00000001
+	ldh ($FF), a
+	
+	ld a, %10000000
+	ldh ($26), a
+	ld a, %01110111
+	ldh ($24), a
+	ld a, %11111111
+	ldh ($25), a
+
+loop:
+	ld a, 40
+pause:
+	halt
+	nop
+	dec a
+	jr nz, pause
+	
+	ld a, %10001000
+	ldh ($11), a
+	ld a, %11110001
+	ldh ($12), a
+	ld a, %00000000
+	ldh ($13), a
+	ld a, %11000100
+	ldh ($14), a
+	jr loop
